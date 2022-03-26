522dc91bebc6886d6845e5cbd54ae9841f12389e
LB <lbutton@riacoding.net>
2022-03-26 15:01:29 -0700

    add README

---
+++ README.md
@@ -0,0 +1,11 @@
+#Dynamic Image Generator
+
+A simple Express based server that dynamically generated images
+
+npm run start - starts server on port 7788
+
+usage:
+/images?title={title}?id={id}?imagenum={imagenum}
+
+example
+http://localhost:7788/image?name=hello%20world&id=1234&imagenum=1
--- server.js
+++ server.js
@@ -1,45 +1,60 @@
-const express = require('express')
-const { registerFont, createCanvas, loadImage } = require('canvas')
-const app = express()
-const port = 3000
-
+const express = require("express");
+const { registerFont, createCanvas, loadImage } = require("canvas");
+const app = express();
+const port = 7788;
 
 // We need to register our font file to be used in canvas
-registerFont('./fonts/Sign-Painter-Regular.ttf', { family: 'signpainter' })
+registerFont("./fonts/DINWeb-Bold.woff", { family: "DINWeb" });
 
-app.get('/header', (req, res) => {
+async function getImage(title, id, imageNum) {
+  // Define the canvas
+  const width = 600; // width of the image
+  const height = 474; // height of the image
+  const canvas = createCanvas(width, height);
+  const context = canvas.getContext("2d");
 
-    // Grab first name from query
-    let firstname = decodeURI(req.query.name) + "!";
+  // Define the font style
+  context.textAlign = "center";
+  context.textBaseline = "top";
+  context.fillStyle = "#FFFFFF";
+  context.font = "50px 'DINWeb' bold";
 
-    // Define the canvas
-    const width = 600 // width of the image
-    const height = 474 // height of the image
-    const canvas = createCanvas(width, height)
-    const context = canvas.getContext('2d')
-
-    // Define the font style
-    context.textAlign = 'center'
-    context.textBaseline = 'top'
-    context.fillStyle = '#FFFFFF'
-    context.font = "70px 'signpainter' bold";
-
+  try {
     // Load and draw the background image first
-    loadImage('./images/background.jpg').then(image => {
+    const image = await loadImage("./images/background4.jpg");
 
-        // Draw the background
-        context.drawImage(image, 0, 0, 600, 474)
+    context.fillRect(0, 0, 600, 474);
 
-        // Draw the text
-        context.fillText(firstname, 300, 150)
+    // Draw the background
+    context.drawImage(image, 0, 0, 600, 474);
 
-        // Convert the Canvas to a buffer
-        const buffer = canvas.toBuffer('image/png')
+    context.fillStyle = "#393939";
+    // Draw the text
+    context.fillText("Title:", 300, 120);
+    context.fillText(title, 300, 170);
+    context.fillText(id, 300, 280);
+    if (imageNum) {
+      context.fillText(imageNum, 300, 330);
+    }
 
-        // Set and send the response as a PNG
-        res.set({ 'Content-Type': 'image/png' });
-        res.send(buffer)
-    })
-})
+    // Convert the Canvas to a buffer
+    const buffer = canvas.toBuffer("image/png");
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
+    return buffer;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+app.get("/image", async (req, res) => {
+  // Grab first name from query
+  let title = decodeURI(req.query.name);
+  let id = "Submission id: " + decodeURI(req.query.id);
+  let imageNum = "Image: " + decodeURI(req.query.imagenum);
+
+  // Set and send the response as a PNG
+  res.set({ "Content-Type": "image/png" });
+  res.send(await getImage(title, id, imageNum));
+});
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
