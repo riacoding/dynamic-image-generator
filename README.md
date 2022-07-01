b8bc45dc87acc044ef1b82a870ad8a3b93a9913f
LB <lbutton@riacoding.net>
2022-07-01 07:26:47 -0700

    changes text alignment and adds color

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Dynamic Image Generator
 
+Node lts/gallium
+
 A simple Express based server that dynamically generated images
 
 npm run start - starts server on port 7788
 
 usage:
-/images?title={title}?id={id}?imagenum={imagenum}
+/images?name={title}?id={id}?imagenum={imagenum}
 
 example
 http://localhost:7788/image?name=hello%20world&id=1234&imagenum=1
--- server.js
+++ server.js
@@ -1,40 +1,49 @@
 const express = require("express");
 const { registerFont, createCanvas, loadImage } = require("canvas");
+const randomColor = require("randomcolor");
 const app = express();
 const port = 7788;
 
 // We need to register our font file to be used in canvas
 registerFont("./fonts/DINWeb-Bold.woff", { family: "DINWeb" });
 
-async function getImage(title, id, imageNum) {
-  // Define the canvas
-  const width = 600; // width of the image
-  const height = 474; // height of the image
+async function getImage(title, id, imageNum, width = 600, height = 474) {
+  console.log(width, height);
   const canvas = createCanvas(width, height);
   const context = canvas.getContext("2d");
 
   // Define the font style
   context.textAlign = "center";
   context.textBaseline = "top";
-  context.fillStyle = "#FFFFFF";
-  context.font = "50px 'DINWeb' bold";
-
+  context.font = "35px 'DINWeb' bold";
+  const colors = randomColor({
+    count: 10,
+    hue: "#00a9a7",
+  });
+  let index = -1;
+  index > colors.length - 1 ? (index = 0) : index++;
   try {
     // Load and draw the background image first
     const image = await loadImage("./images/background4.jpg");
 
-    context.fillRect(0, 0, 600, 474);
+    //draw rectangle with red background
+    context.fillStyle = colors[index];
 
+    context.fillRect(0, 0, width, height);
+
+    context.globalAlpha = 0.3;
     // Draw the background
-    context.drawImage(image, 0, 0, 600, 474);
+    context.drawImage(image, 0, 0, width, height);
 
+    context.globalAlpha = 0.9;
     context.fillStyle = "#393939";
     // Draw the text
-    context.fillText("Title:", 300, 120);
-    context.fillText(title, 300, 170);
-    context.fillText(id, 300, 280);
+    context.textAlign = "center";
+    context.fillText("Title: " + title, width / 2, height / 2 - 100);
+
+    context.fillText(id, width / 2, height / 2);
     if (imageNum) {
-      context.fillText(imageNum, 300, 330);
+      context.fillText(imageNum, width / 2, height / 2 + 100);
     }
 
     // Convert the Canvas to a buffer
@@ -48,13 +57,23 @@ async function getImage(title, id, imageNum) {
 
 app.get("/image", async (req, res) => {
   // Grab first name from query
-  let title = decodeURI(req.query.name);
-  let id = "Submission id: " + decodeURI(req.query.id);
+  let title = decodeURI(req.query.title);
+  let id = "Sub id: " + decodeURI(req.query.id);
   let imageNum = "Image: " + decodeURI(req.query.imagenum);
+  const width = parseInt(decodeURI(req.query.width));
+  const height = parseInt(decodeURI(req.query.height));
+
+  console.log("width in handler", width);
+  console.log("height in handler", height);
 
   // Set and send the response as a PNG
   res.set({ "Content-Type": "image/png" });
-  res.send(await getImage(title, id, imageNum));
+  if (width === NaN || height === NaN) {
+    res.send(await getImage(title, id, imageNum));
+  } else {
+    console.log("calling full getimage");
+    res.send(await getImage(title, id, imageNum, width, height));
+  }
 });
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
